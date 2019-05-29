b5b5a32f9730348630ba43fb57c47301af0c1594
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-29 09:32:38 +0300

    ADded a user model

--- neighbours/models.py
+++ neighbours/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Neighbour(models.Model):
+    name=models.CharField(max_length=250)
+    location=models.CharField(max_length=220)
+    occupationCount=models.IntegerField()
+
+
+
+class Profile(models.Model):
+    fullname=models.CharField(max_length=50)
+    neigbor=models.ForeignKey(Neighbour,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
