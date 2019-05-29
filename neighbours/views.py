4d0fef2a06fae2634a6729a9b0026d1b41c062ef
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-29 10:15:06 +0300

    Created a view function for profile templates

--- neighbours/views.py
+++ neighbours/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .forms import ProfileForm
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def profile(request):
+    form=ProfileForm()
+    return render(request,'profile.html')
