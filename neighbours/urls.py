aa03031d39149038e384014d01bb6d0e75a903a1
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-05-30 10:31:31 +0300

    Changed the navbar content

--- neighbours/urls.py
+++ neighbours/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     url(r'^profile/',views.profile,name='profile'),
     url(r"^edit/",views.edit,name="edit"),
     url(r"^add/business/$",views.business,name='busi'),
+    url(r'^feeds/neighbour/$',views.feeds,name="feeds"),
 ]
